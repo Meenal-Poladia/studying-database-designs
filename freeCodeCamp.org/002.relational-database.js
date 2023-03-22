d5f9c760aae37f396b3d34fb3fd29aab812eeaa6
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-03-22 19:36:39 -0400

    002.relational-database.js

---
+++ freeCodeCamp.org/002.relational-database.js
@@ -0,0 +1,18 @@
+/*
+
+    Relational Database
+
+    In relational database we combine two different attributes.
+
+    Entity is anything about which we store data and attribute is the thing that we store. For eg: Entity can be a
+    person and attribute can be its name or age.
+
+    In relational database we store data in a table.
+
+    Entity type is the category of the entities that we are storing like Person or Caleb. Attribute type is the property
+    which we are storing of the person.
+
+    Attributes are columns and entities are individual rows. All the attributes and entities together make a table
+    which has a name or is called as entity.
+
+ */

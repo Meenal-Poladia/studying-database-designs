e6ee47b3ddb7dc5a7402bb2b2552aa432f0a078b
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-03-22 19:37:49 -0400

    003.rdbms.js

---
+++ freeCodeCamp.org/003.rdbms.js
@@ -0,0 +1,15 @@
+/*
+
+    RDBMS
+
+    DBMS means Database Management System and RDBMS means Relational Database Management System.
+
+    We can create, update, store, get and delete data in the DBMS. We can run queries to access the data in DBMS.
+
+    RDBMS is a sub category of DBMS that is designed to work with relational database.
+    MySQL allows us to build and run a database.
+
+    DBMS helps us to save data and RDBMS helps us to manipulate the data. The files containing data are saved on the
+    server or on the hard drive wherever we want to access the data from.
+
+ */

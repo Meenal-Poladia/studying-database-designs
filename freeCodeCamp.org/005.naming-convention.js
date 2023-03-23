e77ff229384c74147bec434956d9c41b8ac66293
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-03-23 11:57:21 -0400

    005.naming-convention.js

---
+++ freeCodeCamp.org/005.naming-convention.js
@@ -0,0 +1,24 @@
+/*
+
+    Naming Convention
+
+    For writing the queries we use capital letters. -> example 1
+
+    We use _ instead of spaces between 2 words. -> example 2
+
+    The name of the table is usually not written in capital letter -> example 3
+
+    example 1
+    SELECT
+    CREATE
+    UPDATE
+
+    example 2
+    user_name
+    selected_number
+
+    example 3
+    products
+    users
+
+ */

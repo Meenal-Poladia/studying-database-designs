2fd1515f3524577839098312e8f4777df0f92584
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-03-23 11:56:16 -0400

    004.introduction-to-sql.js

---
+++ freeCodeCamp.org/004.introduction-to-sql.js
@@ -0,0 +1,11 @@
+/*
+
+    Introduction to SQL
+
+    SQL is a programming language which is used to communicate with the database. SQL is used to define the database
+    structure and it is used to manipulate the data within.
+
+    DDL is also called as Defining Database Language and DML is Data Manipulation Language. In SQL, queries are
+    written in capital letters.
+
+ */

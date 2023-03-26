293a76f5da68a1824412bd86d5f203525a0d95a4
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-03-25 22:48:21 -0400

    006.what-is-database-design.js

---
+++ freeCodeCamp.org/006.what-is-database-design.js
@@ -0,0 +1,19 @@
+/*
+
+    What is Database Design
+
+    When we are designing database it is important to have data integrity which means all tables have the correct and
+    the latest data and there is no scope of disconnected data. If there is a disconnect then if one table updates then the
+    other table will not update and so the data in the other table will be not be up to date.
+
+    There are 2-3 ways in which database design can be broken down. These are also called as schemas:
+        1. Conceptual
+        2. Logical
+        3. Physical
+
+    From phase 1-3 the schemas goes from being general to specific.
+
+    Database design is a method to separate information over multiple tables rather than having one huge table where we
+    store all the information.
+
+ */

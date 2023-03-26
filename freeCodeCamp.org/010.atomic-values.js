7e1f3ed2fac96de1be1a586161298ee9813b3e59
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-03-26 11:05:08 -0400

    010.atomic-values.js

---
+++ freeCodeCamp.org/010.atomic-values.js
@@ -0,0 +1,8 @@
+/*
+
+    Atomic value
+
+    Atomic value means we just store one thing. For example one column in the table means one thing, while one
+    row gives information about one entity.
+
+ */

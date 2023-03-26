568aa0c164f6170e32023a0a1808f72655be96ab
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-03-26 11:05:35 -0400

    011.relationship.js

---
+++ freeCodeCamp.org/011.relationship.js
@@ -0,0 +1,13 @@
+/*
+
+    Relationship
+
+    In web everything is related to each other. While creating database we can create relationship between tables so
+    that they are related to each other in some way. Relationship talk about entities.
+
+    There can be 3 types of relationships:
+        1. One to One
+        2. One to Many
+        3. Many to Many
+
+ */

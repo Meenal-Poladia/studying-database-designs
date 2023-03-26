d8b6657e783f2c328d10a6f50c96d37bd8fff025
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-03-25 23:03:18 -0400

    008.database-terms.js

---
+++ freeCodeCamp.org/008.database-terms.js
@@ -0,0 +1,61 @@
+/*
+
+    Database Terms
+
+    Data is anything which we store in the database.
+
+    Database is the place where we store our data.
+
+    Relational database is the way in which we store data in tables.
+
+    DBMS is how we manage our database in the code.
+
+    RDBMS is a way in which we control our database using relational database.
+
+    Null is when someone does not enter a value within a column. Null means there is no data into the field.
+
+    Anomalies are errors within our data integrity. For example We wanted to update one column but by mistake we
+    updated 10 columns.
+
+    Integrity, we implement integrity to protect against anomaly. we have 3 ways to keep integrity:
+        1. Entity integrity -  having just one entity with a unique name
+        2. Referential integrity - building connections across tables
+        3. Domain integrity - a column within a table has all of the expected values.
+
+    Entity is something about which we are storing information.
+
+    Attributes are the things we store about the entity.
+
+    Relation is a connection between two sets of data.
+
+    Tuple/ Row are all the attributes about the entity.
+
+    Table is a physical representation of a relation. Within a table we have rows and columns.
+
+    File is another name for a table.
+
+    Record is another name for a row.
+
+    Field is another name for a column.
+
+    Value is the information we put into a specific column.
+
+    Database design is the process of designing your table to remove anomalies and have data integrity.
+
+    Schema is the structure of our database.
+
+    Normalisation are the bunch of steps we will use to get the database design.
+
+    Naming Convention is just a consistency used to make things consistent. We name our database, schema, rows, columns
+    and values in a certain way.
+
+    Key is something to make everything unique within a table. This is how we connect tables. For example we connect
+    2 tables using ids.
+
+    Relation. table and file are all talking about the structure of the table.
+
+    Tuple, rows, entry and record are all talking about rows within a table.
+
+    Columns, attributes and field are all talking about the column within a table.
+
+ */

40d60f1fae6eaa3872c68b9a180592dc05727c1d
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-03-26 11:04:39 -0400

    009.more-database-terms.js

---
+++ freeCodeCamp.org/009.more-database-terms.js
@@ -0,0 +1,29 @@
+/*
+
+    More Database Terms
+
+    SQL means structured Query Language. It is used to connect to the database.
+
+    DDL means Data Definition Language. It is a part of SQL. It is used to defined the structure of the database.
+
+    DML is part of Data Manipulation Language. It is used to insert values in our database. It helps us to search,
+    update and delete data from the database.
+
+    SQL keywords are those which are reserved by the language so that users do not use those defined words. SQL
+    keywords are in capital letters.
+
+    Frontend is what the user sees.
+
+    Backend is what goes behind the scenes.
+
+    Client-Server, the client sends request to the server and servers sends data in the form of response to the
+    client.
+
+    View is showing the data from the server in a way that it is easy for users to understand.
+
+    Joins, views often use joins which basically connects data from multiple tables to create a new table. For example
+    we have a users table and a comments table, the comments table has the comment column, user id which is the foreign
+    key and the id. So, here we want to show the username and the comment rather than the user id so we use join to get
+    the user name using user id from the comments table.
+
+ */

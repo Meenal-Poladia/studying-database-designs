4d1826f2fcec7df25e73491c803afa055034c6fb
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-03-28 10:28:31 -0400

    014.many-to-many-relationship.js

---
+++ freeCodeCamp.org/014.many-to-many-relationship.js
@@ -0,0 +1,9 @@
+/*
+
+    Many to many relationship
+
+    For example - There are multiple classes and multiple students can take multiple classes.
+
+    In relational database, it is usually the one to many and one to one relation which is usually represented.
+
+ */

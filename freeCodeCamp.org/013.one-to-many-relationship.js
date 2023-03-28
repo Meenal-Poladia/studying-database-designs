84fbcb98d69d993a6cf66e63325f789fe81401f8
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-03-28 10:27:48 -0400

    013.one-to-many-relationship.js

---
+++ freeCodeCamp.org/013.one-to-many-relationship.js
@@ -0,0 +1,10 @@
+/*
+
+    One to Many Relationship
+
+    For example - On youtube reviews, one individual can write multiple comments but the comments belongs to only one
+    person at a time.
+
+    Another example is one king having many wives but those wives are not allowed to marry any other person.
+
+ */

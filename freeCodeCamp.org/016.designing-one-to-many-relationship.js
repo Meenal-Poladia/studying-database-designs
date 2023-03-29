109b9b378522a3d9e74e77eacc5fdd4a480a55ae
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-03-29 09:37:56 -0400

    016.designing-one-to-many-relationship.js

---
+++ freeCodeCamp.org/016.designing-one-to-many-relationship.js
@@ -0,0 +1,11 @@
+/*
+
+    Designing one to many relationship
+
+    Example of one to many is, a user can own many cards but one card can only be owned by one user.
+
+    Foreign key is the primary key in another table. What is unique to one table may be many in another table. In a
+    parent child relationship one table which uses the primary key of another table is the foreign key in the child
+    table.
+
+ */

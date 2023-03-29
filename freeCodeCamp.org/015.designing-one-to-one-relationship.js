57a7557eaa9f7e558b4cabb602c86dd90048ccff
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-03-29 09:36:30 -0400

    015.designing-one-to-one-relationship.js

---
+++ freeCodeCamp.org/015.designing-one-to-one-relationship.js
@@ -0,0 +1,15 @@
+/*
+
+    Designing one to one relationship
+
+    In one to one relationship the attribute is exclusive to the entity. The same attribute does not belong to some
+    other entity.
+    For example - Calab Curry can have just one SIN.
+
+    We can have multiple one-to-one connection over different tables. For example - In one table we can store the
+    card holders details and in another table store the card details. We can connect these two tables using the userId.
+
+    In one to one table we focus on the entity model thus we save only one piece of information and store the other
+    information in another table and connect the table using a foreign key.
+
+ */

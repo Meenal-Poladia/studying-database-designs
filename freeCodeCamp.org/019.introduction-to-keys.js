1d2a741cd1f7fea6bb36ac3569f18d5aaaa7e3de
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-03-30 19:21:46 -0400

    019.introduction-to-keys.js

---
+++ freeCodeCamp.org/019.introduction-to-keys.js
@@ -0,0 +1,20 @@
+/*
+
+    Introduction to Keys
+
+    Keys keeps everything unique. Something unique in each row which differentiates it from another is called as a key.
+    For example - email id of each user will be different from one another so there is unique value in each row.
+
+    Two similar keys should not exist in the database. Keys can be a combination of two columns.
+
+    Key should never be changed. If the key of one table changes then we need to update the key in other connecting
+    tables as well.
+
+    Keys can never be null.
+
+    3 important things about keys:
+        1. Keys should always be unique
+        2. Keys should never be changing
+        3. Keys should never be null
+
+ */

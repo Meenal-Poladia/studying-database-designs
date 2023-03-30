2f26d7c781ef5bc55439a2516beee8bf3ab13da3
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-03-30 19:22:47 -0400

    020.primary-key-index.js

---
+++ freeCodeCamp.org/020.primary-key-index.js
@@ -0,0 +1,8 @@
+/*
+
+    Primary Key Index
+
+    Index helps us to find a particular row easily in a large database as we do not have to go through each row
+    manually. Keys are a type of index which can be used to SELECT statement, join them or update a table.
+
+ */

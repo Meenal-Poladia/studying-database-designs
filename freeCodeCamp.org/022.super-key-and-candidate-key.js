447d1fd14c22083c77b3b02e3252b7aabe4df92a
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-03-31 18:41:58 -0400

    022.super-key-and-candidate-key.js

---
+++ freeCodeCamp.org/022.super-key-and-candidate-key.js
@@ -0,0 +1,21 @@
+/*
+
+    SuperKey and Candidate Key
+
+    The two main types of keys are primary keys and foreign keys.
+
+    Superkey is any number of columns that forces the row to be unique.
+
+    Candidate key is the least number of columns needed to force every row to be unique. Candidate keys are candidates
+    for making a primary key.
+
+    Superkey can have many cnadidate keys and other redundant keys as well.
+
+    Candidate key is least and Superkey is any.
+
+    For knowing if the column is a superkey we ask the question:
+        1. Can every row be unique?
+        2. How many columns are needed?
+        3. How many CK do we have?
+
+ */

798041d13beb81c307c044b6689f5c4f3e49cc5b
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-03-31 18:42:48 -0400

    023.primary-key-and-alternate-key.js

---
+++ freeCodeCamp.org/023.primary-key-and-alternate-key.js
@@ -0,0 +1,15 @@
+/*
+
+    Primary Key and Alternate Key
+
+    When we want to choose a candidate key to be a primary key we look for the following features:
+        1. Key should be unique
+        2. Key should never change
+        3. Key should never be null
+
+    Primary key is a source of Index.
+
+    Alternate keys are all the candidate keys that were not selected as primary keys. Alternate keys are not must but
+    it is good to have them as they can help in searches, or we can create an index.
+
+ */

d126d8b869aed6093a6ee9d9c25ecd040dcb5ac1
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-04-01 08:53:35 -0400

    025.should-i-use-surrogate-keys-or-natural-keys.js

---
+++ freeCodeCamp.org/025.should-i-use-surrogate-keys-or-natural-keys.js
@@ -0,0 +1,12 @@
+/*
+
+    Should I use surrogate keys or natural keys
+
+    In natural keys, we do not manually need to define them. Surrogate keys are added manually and have no real meaning.
+
+    It is good to have a natural key or something like a natural key to use with every table.
+
+    Whichever key we pick we need to use it all across the database. It is not a good practise to keep changing from
+    natural key to surrogate key.
+
+ */

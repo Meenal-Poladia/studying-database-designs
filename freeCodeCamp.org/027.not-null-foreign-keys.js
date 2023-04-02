b33e89a0ca4075a91e25816a1297f6186a98a24b
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-04-02 10:22:46 -0400

    027.not-null-foreign-keys.js

---
+++ freeCodeCamp.org/027.not-null-foreign-keys.js
@@ -0,0 +1,15 @@
+/*
+
+    Not Null Foreign keys
+
+    Not Null means a every single row has to have a value. Primary keys are required all the time, they are never
+    changing and never null.
+
+    Foreign keys values can change as references can change but primary keys remain the same always.
+    For example a User at first owns a toyota but later sells the current car and buys Honda car. Here the referencing
+    to the car changed but the primary table consisting of the car details remain the same.
+
+    Foreign key values can be null but primary key can never be null. For example - A user can sell ones car and not
+    own a car at all.
+
+ */

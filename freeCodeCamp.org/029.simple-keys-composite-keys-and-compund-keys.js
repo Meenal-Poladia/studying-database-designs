9d4642e539e01f4f8d1f6ebd70a55d19f231f388
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-04-02 11:27:26 -0400

    029.simple-keys-composite-keys-and-compund-keys.js

---
+++ freeCodeCamp.org/029.simple-keys-composite-keys-and-compund-keys.js
@@ -0,0 +1,12 @@
+/*
+
+    Simple keys composite keys and compound keys
+
+    Simple means the key contains of one column for example surrogate key. Composite key means the key contains of two
+    or more columns. For example - natural keys are combination of two or more columns.
+    Some database dont differentiate between composite and compound keys and us it interchangeably.
+
+    Compound keys are those keys in which the table have multiple columns and they are all keys themselves. The most
+    common example of this is the intermediary table.
+
+ */
